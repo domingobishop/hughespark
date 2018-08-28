86d621a6a53302cd028efb1f8940d2747a7cf74a
Chris Bishop <domingobishop@gmail.com>
2018-08-28 12:15:50 +0100

    config theme

--- footer.php
+++ footer.php
@@ -1,10 +1,13 @@
 <footer id="footer" class="footer">
   <div class="container">
-    <div class="row">
+<div class="row">
+	<div class="col-md-6">
+	</div>
+	<div class="col-md-6">
+	</div>
       <div class="col-md-12 text-center site-info">
-        <p><small>Copyright © <?php echo date("Y"); ?> <br>
-           Blank Canvas with Bootstrap <br>
-          Theme by <a href="http://chrisbishop.me.uk/" target="_blank">Chris Bishop</a></small></p>
+        <p><small>Hughes Park copyright © <?php echo date("Y"); ?> <br>
+          Website by <a href="http://creatistic.com.au/" target="_blank">Creatistic/a></small></p>
       </div>
     </div>
   </div>
